a10d613a6cca1abf1ef5b161596f42a6eb96aa46
Dustin Zeisler <dustin@zive.me>
2013-07-19 09:52:19 -0700

    passing tests

--- wordfind.rb
+++ wordfind.rb
@@ -1,6 +1,11 @@
 def find_words( puzzle, words)
 	results = Array.new()
 
+  result = go_east(puzzle, words)
+  results << result unless result == false
+
+  result = go_west(puzzle, words)
+  results << result unless result == false
 	# puzzle is an array of strings
 	# words is an array of strings
 
@@ -10,6 +15,21 @@ def find_words( puzzle, words)
 	# WORD(row,column,direction) format
 
 	# your code here!
-
 	return results
+end
+
+def go_east(puzzle, words)
+  if puzzle.first.scan words.first
+    return "#{words[0]}(1,1,east)"
+  else
+    return false
+  end
+end
+
+def go_west(puzzle, words)
+  if puzzle.first.reverse.scan words.first
+    return "#{words[0]}(1,1,west)"
+  else
+    return false
+  end
 end
